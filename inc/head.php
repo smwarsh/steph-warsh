bc3c82f569ac05ce688824647898f5bcebb87408
Steph <stephaniewarsh@gmail.com>
2020-05-31 13:03:07 -0400

    Remove old favicon + rename current favicon to favicon.png + update the reference to it in head.php

--- inc/head.php
+++ inc/head.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="css/styles.css">
 
-<link rel="icon" type="image/png" href="letter-s-white.png">
+<link rel="icon" type="image/png" href="favicon.png">
 <!--<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>-->
 
 
