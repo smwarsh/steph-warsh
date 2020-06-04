e549aaa75fabf2b28e36ad98c36ae893e2e78d70
Steph <stephaniewarsh@gmail.com>
2020-06-04 18:54:40 -0400

    Remove PHP from Warner portfolio details page

--- portfolio/warner.php
+++ portfolio/warner.php
@@ -7,7 +7,16 @@
 
 		<title>Warner School | Steph Warsh | Web Developer</title>
 
-		<?php include '../inc/head.php'; ?>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="../css/styles.css">
+
+		<link rel="icon" type="image/png" href="../favicon.png">
+		<!--<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>-->
+
+
+		<style>
+			@import 'https://fonts.googleapis.com/css?family=Poiret+One|Sacramento';
+		</style>
 
 	</head>
 
