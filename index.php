225631ec9fe657c62bb0371137e7dfc7fc5f47b6
Steph <stephaniewarsh@gmail.com>
2020-06-04 21:36:54 -0400

    Fix home page styles to be mobile-first and mobile-friendly + darken the background a little to increase readability

--- index.php
+++ index.php
@@ -11,55 +11,34 @@
 		
 	</head>
 
-	<body>
+	<body class="home-page">
 
 		<?php 
 			$page = 'home';
 			include 'inc/nav.php';
 		?>
 
-		<div class="img-bg parallax">
-
-			<!-- <nav id="resources">
-				<ul>
-					<li><a href="StephWarshResume.pdf" title="resume" target="_blank"><img src="img/document-128.png" alt="resume" width="40" height="40"></a></li>
-					<li><a href="portfolio.html" title="portfolio"><img src="img/folder-128.png" alt="portfolio" width="40" height="40"></a></li>
-					<li><a href="https://github.com/swarsh" title="github" target="_blank"><img src="img/github2.png" alt="github mark" width="40" height="40"></a></li>
-					<li><a href="https://www.flickr.com/photos/67760352@N05/albums" title="flickr" target="_blank"><img src="img/flickr2.png" width="40" height="40" alt="flickr icon"></a></li>
-					<li><a href="https://www.linkedin.com/in/stephaniewarsh" title="linkedin" target="_blank"><img src="img/linkedin2.png" alt="linkedin icon" width="40" height="40"></a></li>
-				</ul>
-			</nav> --><!-- #resources -->
-
-			<!--<nav>
-				<ul>
-					<li><a href="#">home</a></li>
-					<li><a href="about.html">about</a></li>
-					<li><a href="#">contact</a></li>
-				</ul>
-			</nav>-->
-			
-			<div id="title">
-				<h1>steph warsh</h1>
-				<p>front-end web developer</p>
-			
-
-			<div id="resources">
-				<ul>
-					<li><a href="files/StephWarshResumeMar2019.pdf" title="resume" target="_blank"><img src="img/document-128.png" alt="resume" width="40" height="40"></a></li>
-					<!-- note to self: remember to add credit when adding external link icon
-					<a target="_blank" href="https://icons8.com/icons/set/external-link">External Link icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>  -->
-					<li><a href="portfolio.php" title="portfolio"><img src="img/folder-128.png" alt="portfolio" width="40" height="40"></a></li>
-					<li><a href="https://github.com/smwarsh" title="github" target="_blank"><img src="img/github2.png" alt="github mark" width="40" height="40"></a></li>
-					<li><a href="https://www.linkedin.com/in/stephwarsh" title="linkedin" target="_blank"><img src="img/linkedin2.png" alt="linkedin icon" width="40" height="40"></a></li>
-				</ul>
-			</div><!-- #resources -->
-
-</div><!-- #title -->
-		</div> <!-- .parallax -->
-
-		<div id="about">
-			
-		</div>
+    <div class="content">
+      <div class="page">
+        <div id="title">
+          <h1>steph warsh</h1>
+          <h2>front-end web developer</h2>
+        
+  
+          <div id="resources">
+            <ul>
+              <li><a href="files/StephWarshResumeMar2019.pdf" title="resume" target="_blank"><img src="img/document-128.png" alt="resume" width="40" height="40"></a></li>
+              <!-- note to self: remember to add credit when adding external link icon
+              <a target="_blank" href="https://icons8.com/icons/set/external-link">External Link icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>  -->
+              <li><a href="portfolio.php" title="portfolio"><img src="img/folder-128.png" alt="portfolio" width="40" height="40"></a></li>
+              <li><a href="https://github.com/smwarsh" title="github" target="_blank"><img src="img/github2.png" alt="github mark" width="40" height="40"></a></li>
+              <li><a href="https://www.linkedin.com/in/stephwarsh" title="linkedin" target="_blank"><img src="img/linkedin2.png" alt="linkedin icon" width="40" height="40"></a></li>
+            </ul>
+          </div><!-- #resources -->
+  
+        </div><!-- #title -->
+      </div><!-- .page -->
+    </div><!-- .content -->
 
 	</body>
 
