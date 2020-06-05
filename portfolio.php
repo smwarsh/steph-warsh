ee5e8e14ebbe37ecc398934953a710cb1e5685cd
Steph <stephaniewarsh@gmail.com>
2020-06-04 22:21:33 -0400

    Some more global styles but mostly redoing the layout of the Portfolio page

--- portfolio.php
+++ portfolio.php
@@ -11,68 +11,92 @@
 	
 	</head>
 
-	<body class="sub-page">
+	<body class="subp portfolio">
 
 		<?php 
 			$page = 'portfolio';
 			include 'inc/nav.php';
 		?>
 
-		<div class="container" id="portfolio">
-
-			<h1>My Work</h1>
-
-			<div class="white-bg">
-
-				<h2>Projects</h2>
-
-				<div class="work-item">
-					<a href="https://github.com/smwarsh/thrift" target="_blank"><img src="img/dollars_sm.jpg" alt="Thrift Spending Tracker"></a><!--
-					--><div class="description">
-					<h3>Thrift Spending Tracker</h3>
-					<p>JavaScript, HTML templates (Pug/Jade), Git, Node.js, Express, MongoDB</p>
-					<p>I am developing a spending tracker web application to replace a tedious spreadsheet with which I am currently tracking my expenses.</p>
-					<p class="click"><a href="https://github.com/smwarsh/thrift" target="_blank">See it on GitHub &#8594;</a></p>
-					</div><!-- .description -->
-				</div><!-- .work-item -->
-
-				<div class="work-item">
-					<a href="https://github.com/smwarsh/challenge-channel" target="_blank"><img src="img/challengechannel_sm.jpg" alt="Challenge Channel"></a><!--
-					--><div class="description">
-					<h3>Challenge Channel</h3>
-					<p>Design, HTML, CSS, SASS, Bootstrap, GitHub</p>
-					<p>I worked with a team to complete this web app for a web programming course. I designed the layout and coded all the HTML and CSS for the project.</p>
-					<p class="click"><a href="https://github.com/smwarsh/challenge-channel" target="_blank">See it on GitHub &#8594;</a></p>
-					</div><!-- .description -->
-				</div><!-- .work-item -->
-
-				<h2>Experience</h2>
-
-				<div class="work-item">
-					<a href="https://greaterthanone.com/" target="_blank"><img src="img/gto-logo.png" alt="Web development at Greater Than One digital agency"></a><!--
-					--><div class="description">
-					<h3>Greater Than One</h3>
-					<p>JavaScript, jQuery, HTML, CSS, Drupal CMS</p>
-					<p>I am given a design file and behavior information and I code the website to mirror it exactly. I pride myself on my clean, organized code for mobile-first sites that are responsive and work well across all major browsers for mobile and desktop devices, including Google Chrome, Firefox, Safari, Edge, and Internet Explorer 11.</p>
-					<p class="click"><a href="https://greaterthanone.com/" target="_blank">Visit the company site &#8594;</a></p>
-					</div><!-- .description -->
-				</div><!-- .work-item -->
-
-				<div class="work-item">
-					<a href="portfolio/warner.php"><img src="img/warner_sm.jpg" alt="Warner"></a><!--
-					--><div class="description">
-					<h3>Warner School of Education</h3>
-					<p>Design, HTML, CSS, PHP, CMS</p>
-					<p>I enhanced the website for the graduate school of education at the University of Rochester, transferred content from the old site to the new site, and designed layouts.</p>
-					<p class="click"><a href="portfolio/warner.php">Learn more &#8594;</a></p>
-					</div><!-- .description -->
-				</div><!-- .work-item -->
-
-			</div> <!-- .white-bg -->
-		<div id="about">
-			
-		</div><!-- .about -->
-	</div><!-- .container -->
+		<div class="content">
+		  <div class="page" id="portfolio">
+  
+  			<h1>My Work</h1>
+  
+  			<div class="white-bg">
+  
+  				<h2>Projects</h2>
+  
+  				<div class="work-item">
+            <div class="image">
+              <a href="https://github.com/smwarsh/thrift" target="_blank">
+                <img src="img/dollars_sm.jpg" alt="Thrift Spending Tracker">
+              </a>
+            </div>          
+            <div class="description">
+              <h3>Thrift Spending Tracker</h3>
+              <p>JavaScript, HTML templates (Pug/Jade), Git, Node.js, Express, MongoDB</p>
+              <p>I am developing a spending tracker web application to replace a tedious spreadsheet with which I am currently tracking my expenses.</p>
+              <p class="click">
+                <a href="https://github.com/smwarsh/thrift" target="_blank">See it on GitHub &#8594;</a>
+              </p>
+  					</div><!-- .description -->
+  				</div><!-- .work-item -->
+  
+  				<div class="work-item">
+            <div class="image">
+              <a href="https://github.com/smwarsh/challenge-channel" target="_blank">
+                <img src="img/challengechannel_sm.jpg" alt="Challenge Channel">
+              </a>
+            </div>
+            <div class="description">
+              <h3>Challenge Channel</h3>
+              <p>Design, HTML, CSS, SASS, Bootstrap, GitHub</p>
+              <p>I worked with a team to complete this web app for a web programming course. I designed the layout and coded all the HTML and CSS for the project.</p>
+              <p class="click">
+                <a href="https://github.com/smwarsh/challenge-channel" target="_blank">See it on GitHub &#8594;</a>
+              </p>
+  					</div><!-- .description -->
+  				</div><!-- .work-item -->
+  
+  
+  				<h2>Experience</h2>
+  
+  				<div class="work-item">
+            <div class="image">
+              <a href="https://greaterthanone.com/" target="_blank">
+                <img src="img/gto-logo.png" alt="Web development at Greater Than One digital agency">
+              </a>
+            </div>
+            <div class="description">
+              <h3>Greater Than One</h3>
+              <p>JavaScript, jQuery, HTML, CSS, Drupal CMS</p>
+              <p>I am given a design file and behavior information and I code the website to mirror it exactly. I pride myself on my clean, organized code for mobile-first sites that are responsive and work well across all major browsers for mobile and desktop devices, including Google Chrome, Firefox, Safari, Edge, and Internet Explorer 11.</p>
+              <p class="click">
+                <a href="https://greaterthanone.com/" target="_blank">Visit the company site &#8594;</a>
+              </p>
+  					</div><!-- .description -->
+  				</div><!-- .work-item -->
+  
+  				<div class="work-item">
+            <div class="image">
+              <a href="portfolio/warner.php">
+                <img src="img/warner_sm.jpg" alt="Warner">
+              </a>
+            </div>          
+            <div class="description">
+              <h3>Warner School of Education</h3>
+              <p>Design, HTML, CSS, PHP, CMS</p>
+              <p>I enhanced the website for the graduate school of education at the University of Rochester, transferred content from the old site to the new site, and designed layouts.</p>
+              <p class="click">
+                <a href="portfolio/warner.php">Learn more &#8594;</a>
+              </p>
+  					</div><!-- .description -->
+  				</div><!-- .work-item -->
+  
+  			</div> <!-- .white-bg -->
+  	  </div><!-- .page -->
+		</div><!-- .content -->
 	</body>
 
 </html>
